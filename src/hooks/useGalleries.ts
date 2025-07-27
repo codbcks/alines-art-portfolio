4bedb3f048cab2beff34be1e298d7e9107e508c5
codbcks <euchan1234@gmail.com>
2025-07-27 14:31:25 +1200

    error handling in useGalleries hook

--- src/hooks/useGalleries.ts
+++ src/hooks/useGalleries.ts
@@ -1,17 +1,33 @@
-import { useEffect, useState } from 'react';
 import { Gallery } from '@/types/Gallery';
+import { useEffect, useState } from 'react';
 import { getGalleries } from '@/services/client/galleryClientService';
 
 export function useGalleries() {
   const [galleries, setGalleries] = useState<Gallery[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    getGalleries().then((data) => {
-      setGalleries(data);
-      setLoading(false);
-    });
+    let isMounted = true;
+
+    getGalleries()
+      .then((data) => {
+        if (isMounted) {
+          setGalleries(data);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
-  return { galleries, loading };
+  return { galleries, loading, error };
 }
